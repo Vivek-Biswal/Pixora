5b09c6cd7a200022b58beeba557004ac670887b3
vb <vivekbiswal2006@gmail.com>
2026-05-23 23:45:48 +0530

    style(icons): update Gmail logo to monochrome line-art to match other social icons

--- src/components/SocialIcons.jsx
+++ src/components/SocialIcons.jsx
@@ -54,17 +54,20 @@ export const GithubIcon = ({ size = 24, color = 'currentColor', className = '' }
   </svg>
 );
 
-export const GmailIcon = ({ size = 24, className = '' }) => (
+export const GmailIcon = ({ size = 24, color = 'currentColor', className = '' }) => (
   <svg 
     xmlns="http://www.w3.org/2000/svg" 
     width={size} 
     height={size} 
     viewBox="0 0 24 24" 
+    fill="none" 
+    stroke={color} 
+    strokeWidth="2" 
+    strokeLinecap="round" 
+    strokeLinejoin="round" 
     className={className}
   >
-    <path fill="#ea4335" d="M24 5.5v13c0 1.1-.9 2-2 2H18V10.12l-6 4.5-6-4.5V20.5H4c-1.1 0-2-.9-2-2v-13c0-.84.53-1.58 1.3-1.87L12 9.42l8.7-5.79c.77.29 1.3 1.03 1.3 1.87z"/>
-    <path fill="#c5221f" d="M2.68 3.32L12 9.42l9.32-6.1A2 2 0 0 0 19.5 2h-15a2 2 0 0 0-1.82 1.32z"/>
-    <path fill="#fbbc04" d="M24 5.5v4.62l-6 4.5V20.5H24v-15z"/>
-    <path fill="#4285f4" d="M0 5.5v4.62l6 4.5V20.5H0v-15z"/>
+    <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+    <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
   </svg>
 );
